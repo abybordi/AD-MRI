1d7562d1bf30a73492ba8bb917315a19029f7c72
Daniel Brennan <daniel@Chet.local>
2020-05-23 22:25:45 -0400

    updated pipeline with preproc

---
+++ .ipynb_checkpoints/Preprocess-checkpoint.py
@@ -0,0 +1,10 @@
+# Here is the plan:
+
+# 1. determine optimal scan ID's for each participant. (scan_ID.py)
+
+# 2. Try to download batch of 20 scans
+    #2a. see which scans were downloaded. Append not downloaded scans to "bad" csv
+
+# 3. load and preprocess (spatial + intensity normalize)
+
+# 4. visual inspection.py, ask for response "good" or "bad"
